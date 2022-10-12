b93a35840c7e66213ad108d4db1a232618beebd1
sean stephen brian <seanstephenbrian@gmail.com>
2022-10-12 14:50:39 -0400

    Put initial page load function in separate module

--- dist/main.js
+++ dist/main.js
@@ -7,26 +7,90 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("const generateInitialStructure = (function() {\n\n    const body = document.querySelector('body');\n\n    // create header section:\n    const header = document.createElement('header');\n    body.appendChild(header);\n\n        header.textContent = 'HEADER';\n\n    // create nav div:\n    const nav = document.createElement('div');\n    nav.classList.add('nav');\n    body.appendChild(nav);\n\n        nav.textContent = 'NAV';\n\n    // create nav buttons & append them to nav:\n    const menuButton = document.createElement('div');\n    menuButton.classList.add('menu-button');\n    menuButton.classList.add('nav-button');\n    nav.appendChild(menuButton);\n\n        menuButton.textContent = 'MENU BUTTON';\n\n    const specialsButton = document.createElement('div');\n    specialsButton.classList.add('specials-button');\n    specialsButton.classList.add('nav-button');\n    nav.appendChild(specialsButton);\n\n        specialsButton.textContent = 'SPECIALS BUTTON';\n\n    const aboutButton = document.createElement('div');\n    aboutButton.classList.add('about-button');\n    aboutButton.classList.add('nav-button');\n    nav.appendChild(aboutButton);\n\n        aboutButton.textContent = 'ABOUT BUTTON';\n    \n    // create main content div:\n    const content = document.createElement('div');\n    content.id = 'content';\n    body.appendChild(content);\n\n        content.textContent = 'CONTENT';\n\n    // create footer:\n    const footer = document.createElement('footer');\n    body.appendChild(footer);\n\n        footer.textContent = 'FOOTER';\n\n})();\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _init_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./init.js */ \"./src/init.js\");\n\n\n(0,_init_js__WEBPACK_IMPORTED_MODULE_0__.generateInitialStructure)();\n\n//# sourceURL=webpack://restaurant-page/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/init.js":
+/*!*********************!*\
+  !*** ./src/init.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"generateInitialStructure\": () => (/* binding */ generateInitialStructure)\n/* harmony export */ });\nconst generateInitialStructure = (function() {\n\n    const body = document.querySelector('body');\n\n    // create header section:\n    const header = document.createElement('header');\n    body.appendChild(header);\n\n        header.textContent = 'HEADER';\n\n    // create nav div:\n    const nav = document.createElement('div');\n    nav.classList.add('nav');\n    body.appendChild(nav);\n\n        nav.textContent = 'NAV';\n\n    // create nav buttons & append them to nav:\n    const menuButton = document.createElement('div');\n    menuButton.classList.add('menu-button');\n    menuButton.classList.add('nav-button');\n    nav.appendChild(menuButton);\n\n        menuButton.textContent = 'MENU BUTTON';\n\n    const specialsButton = document.createElement('div');\n    specialsButton.classList.add('specials-button');\n    specialsButton.classList.add('nav-button');\n    nav.appendChild(specialsButton);\n\n        specialsButton.textContent = 'SPECIALS BUTTON';\n\n    const aboutButton = document.createElement('div');\n    aboutButton.classList.add('about-button');\n    aboutButton.classList.add('nav-button');\n    nav.appendChild(aboutButton);\n\n        aboutButton.textContent = 'ABOUT BUTTON';\n    \n    // create main content div:\n    const content = document.createElement('div');\n    content.id = 'content';\n    body.appendChild(content);\n\n        content.textContent = 'CONTENT';\n\n    // create footer:\n    const footer = document.createElement('footer');\n    body.appendChild(footer);\n\n        footer.textContent = 'FOOTER';\n\n})();\n\n\n\n//# sourceURL=webpack://restaurant-page/./src/init.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
