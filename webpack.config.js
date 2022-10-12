d10c08ee83899e8a319421b8dd9ffe87c486ead2
sean stephen brian <seanstephenbrian@gmail.com>
2022-10-12 15:59:02 -0400

    Lay out basic CSS page design using flex

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'restaurant page | sean stephen brian',
+        }),
+    ],
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    optimization: {
+        runtimeChunk: 'single',
     },
 };
